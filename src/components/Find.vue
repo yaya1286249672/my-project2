26bf026a96b8f285149af6755738a7906a217d20
yaya <1286249672@qq.com>
2018-01-19 16:36:31 +0800

    组件通信

--- src/components/Find.vue
+++ src/components/Find.vue
@@ -1,18 +1,46 @@
 <template>
   <div>
-    <mt-search :value.sync="value"></mt-search>
+    <mt-search
+      autofocus
+      v-model="value"
+      :result="filterResult">
+    </mt-search>
   </div>
 </template>
 <script>
   import Vue from 'vue'
-  import { Search } from 'mint-ui';
+  import {Search} from 'mint-ui';
   Vue.component(Search.name, Search);
   export default{
-      name : 'find',
-      data (){
-          return{
-              value:''
-          }
+    name: 'find',
+    data (){
+      return {
+        value: '',
+        // 默认数据
+        defaultResult: [
+          'Apple',
+          'Banana',
+          'Orange',
+          'Durian',
+          'Lemon',
+          'Peach',
+          'Cherry',
+          'Berry',
+          'Core',
+          'Fig',
+          'Haw',
+          'Melon',
+          'Plum',
+          'Pear',
+          'Peanut',
+          'Other'
+        ]
+      };
+    },
+    computed: {
+      filterResult() {
+        return this.defaultResult.filter(value => new RegExp(this.value, 'i').test(value));
       }
+    }
   }
 </script>
